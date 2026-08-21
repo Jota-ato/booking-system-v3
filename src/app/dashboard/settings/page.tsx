fd25b10b7975a1760853a07359e5f1998b33f846
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-08-21 10:44:18 -0600

    feat: refactor settings page layout for improved structure and user details display

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -15,25 +15,27 @@ export default async function SettingsPage() {
     <>
       <SettingsPagesSection>
         <h2>Profile</h2>
-        <div className="flex">
-          <Card className="flex-1">
-            <CardContent className="flex flex-row gap-6">
-              {/** Controls  */}
-              <div className="flex flex-1 flex-col items-start gap-2">
-                <h3>User data</h3>
+        <Card>
+          <CardContent className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {/** Controls  */}
+            <div className="flex flex-col items-start gap-2">
+              <h3>User details</h3>
+              <div className="flex-1 flex flex-col gap-2">
                 <p>
                   Role: <Badge className="ml-2">{user.role}</Badge>
                 </p>
                 <p className="text-muted-foreground">
                   Set your account details
                 </p>
-                <Button variant="outline" size="lg">
-                  <PenSquare className="size-4" />
-                  Edit
-                </Button>
               </div>
-              {/** Profile data */}
-              <div className="flex flex-col gap-2">
+              <Button variant="outline" size="lg">
+                <PenSquare className="size-4" />
+                Edit
+              </Button>
+            </div>
+            {/** Profile data */}
+            <div className="md:col-span-2 flex flex-col md:flex-row gap-6">
+              <div className="flex-1 flex flex-col gap-2 md:border-l border-border md:pl-6">
                 <div className="flex flex-col items-start gap-2">
                   <span>Name</span>
                   <span className="w-full px-4 py-2 border border-border rounded-md">
@@ -55,21 +57,24 @@ export default async function SettingsPage() {
                   width={100}
                   height={100}
                   loading="eager"
-                  priority
                   className="rounded-full"
                 />
-                <div className="flex gap-2">
+                <div className="flex items-center gap-2">
                   <Button variant="outline" size="sm">
                     Change picture
                   </Button>
-                  <Button variant="outline" size="icon">
+                  <Button
+                    aria-label="Delete profile picture"
+                    variant="outline"
+                    size="icon-sm"
+                  >
                     <Trash className="size-4" />
                   </Button>
                 </div>
               </div>
-            </CardContent>
-          </Card>
-        </div>
+            </div>
+          </CardContent>
+        </Card>
       </SettingsPagesSection>
     </>
   );
