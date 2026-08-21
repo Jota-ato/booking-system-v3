e136c4e9aa9de29d38209969cf4b03ef690b43df
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-08-21 10:46:31 -0600

    feat: split account details into its own component

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,10 +1,6 @@
+import { AccountDetailsCard } from "@/features/users/components/account-details-card";
 import { requireAuth } from "@/lib/auth-server";
-import { SettingsPagesSection } from "@/shared/components/dashboard/settings/settings-pages-section";
-import { Badge } from "@/shared/components/ui/badge";
-import { Button } from "@/shared/components/ui/button";
-import { Card, CardContent } from "@/shared/components/ui/card";
-import { PenSquare, Trash } from "lucide-react";
-import Image from "next/image";
+
 import { redirect } from "next/navigation";
 
 export default async function SettingsPage() {
@@ -13,69 +9,7 @@ export default async function SettingsPage() {
 
   return (
     <>
-      <SettingsPagesSection>
-        <h2>Profile</h2>
-        <Card>
-          <CardContent className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {/** Controls  */}
-            <div className="flex flex-col items-start gap-2">
-              <h3>User details</h3>
-              <div className="flex-1 flex flex-col gap-2">
-                <p>
-                  Role: <Badge className="ml-2">{user.role}</Badge>
-                </p>
-                <p className="text-muted-foreground">
-                  Set your account details
-                </p>
-              </div>
-              <Button variant="outline" size="lg">
-                <PenSquare className="size-4" />
-                Edit
-              </Button>
-            </div>
-            {/** Profile data */}
-            <div className="md:col-span-2 flex flex-col md:flex-row gap-6">
-              <div className="flex-1 flex flex-col gap-2 md:border-l border-border md:pl-6">
-                <div className="flex flex-col items-start gap-2">
-                  <span>Name</span>
-                  <span className="w-full px-4 py-2 border border-border rounded-md">
-                    {user.name}
-                  </span>
-                </div>
-                <div className="flex flex-col items-start gap-2">
-                  <span>Email</span>
-                  <span className="w-full px-4 py-2 border border-border rounded-md">
-                    {user.email}
-                  </span>
-                </div>
-              </div>
-              {/** Profile picture */}
-              <div className="flex flex-col items-center gap-2">
-                <Image
-                  src={user.image!}
-                  alt="Profile picture"
-                  width={100}
-                  height={100}
-                  loading="eager"
-                  className="rounded-full"
-                />
-                <div className="flex items-center gap-2">
-                  <Button variant="outline" size="sm">
-                    Change picture
-                  </Button>
-                  <Button
-                    aria-label="Delete profile picture"
-                    variant="outline"
-                    size="icon-sm"
-                  >
-                    <Trash className="size-4" />
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-      </SettingsPagesSection>
+      <AccountDetailsCard user={user} />
     </>
   );
 }
